63f53080b7c08248325f0e06b8cd937667fe22a6
Sherry Wu <shewu19@gmail.com>
2012-02-04 23:23:06 -0500

    yay resizing sort of works

--- js/wb.js
+++ js/wb.js
@@ -1,6 +1,8 @@
 function resizeViewport() {
 	var screenViewportHeight = document.documentElement.clientHeight;
 	// keep these two values in sync with the css kthx
+	console.log("headerHeight = " + document.getElementsByTagName('header')[0].style.height);
+	console.log("footerHeight = " + document.getElementsByTagName('footer')[0].style.height);
 	var headerHeight = 16;
 	var footerHeight = 10;
 	document.getElementById('viewport').style.height = screenViewportHeight - headerHeight - footerHeight + 'px';
