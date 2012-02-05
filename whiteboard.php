3b0d52142d84b45e3213743f2026811ac578569c
Brian C Hamrick <bhamrick@mit.edu>
2012-02-05 13:35:55 -0500

    typo

--- whiteboard.php
+++ whiteboard.php
@@ -58,7 +58,7 @@ whiteboard_id = <?php echo "$whiteboard_id"; ?>;
 </div>
 
 <div id=objmenu class=vmenu>
-<div class=firstLi onclick="objectDeleteMenuhandler(event)">Remove</div>
+<div class=firstLi onclick="objectDeleteMenuHandler(event)">Remove</div>
 </div>
 
 <div class=overlayLightbox>
