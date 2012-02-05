9b91ebf0b7dc02875141a4adc5a9a9dd49c53db6
Sherry Wu <shewu19@gmail.com>
2012-02-05 09:09:16 -0500

    changed get to post for image uploads

--- whiteboard.php
+++ whiteboard.php
@@ -88,7 +88,7 @@ whiteboard_id = <?php echo "$whiteboard_id"; ?>;
 <div class=overlayLightbox>
 <div class=overlayContent>
 <h3>Add an image</h3>
-<form id=imgUploadForm action="<?php echo $PHP_SELF;?>" method=GET>
+<form id=imgUploadForm action="<?php echo $PHP_SELF;?>" method=POST>
 <ul>
 <li>Upload an image: <input type=file id=imgUploadFile name=imgUpload size=1024 /></li>
 <li>Paste from source: <input type=text id=imgUploadURL name=imgURL /></li>
