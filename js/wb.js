8c32b1edd2db1b6129390b10a7f0420b461556e2
Travis Hance <tjhance7@gmail.com>
2012-02-05 12:43:31 -0500

    going to test stuff with console.log

--- js/wb.js
+++ js/wb.js
@@ -341,6 +341,7 @@ $(document).ready(function() {
 		dragged = false;
 	})
 	.mouseup(function(e) {
+		console.log("hello, in mouseup: dragged = " + dragged + ", dragObject = " + dragObject);
 		isMouseDown = false;
 		if(dragged && dragObject != null) {
 			sendMoveUpdate(getObjFromDiv(dragObject), dragObjectBaseX + e.pageX - dragBaseX, dragObjectBaseY + e.pageY - dragBaseY);
