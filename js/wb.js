de5e7f0b1db12368394c7c72ae70da7dd0c846c4
Sherry Wu <shewu19@gmail.com>
2012-02-05 07:10:37 -0500

    hides menu for all handlers

--- js/wb.js
+++ js/wb.js
@@ -374,12 +374,14 @@ function textMenuHandler(e) {
 
 function imageMenuHandler(e) {
 	e.stopPropagation();
+	$('#vmenu').css('display', 'none');
 	alert('image');
 	return false;
 }
 
 function hamburgerMenuHandler(e) {
 	e.stopPropagation();
+	$('#vmenu').css('display', 'none');
 	alert('hamburger!');
 	return false;
 }
