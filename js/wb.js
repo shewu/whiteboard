60fb275b0e5267e10b9c7fc454cbbcc24e10a44f
Sherry Wu <shewu19@gmail.com>
2012-02-04 23:14:16 -0500

    hardcoded heights into js

--- js/wb.js
+++ js/wb.js
@@ -1,7 +1,8 @@
 function resizeViewport() {
 	var screenViewportHeight = document.documentElement.clientHeight;
-	var headerHeight = document.getElementsByTagName('header')[0].style.height;
-	var footerHeight = document.getElementsByTagName('footer')[0].style.height;
+	// keep these two values in sync with the css kthx
+	var headerHeight = 16;
+	var footerHeight = 10;
 	document.getElementById('viewport').style.height = screenViewportHeight - headerHeight - footerHeight;
 	return;
 }
