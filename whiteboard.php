e9658a508438e96e9aad61b7bb4e559d14998460
Sherry Wu <shewu19@gmail.com>
2012-02-05 10:18:12 -0500

    even moar debug

--- whiteboard.php
+++ whiteboard.php
@@ -6,7 +6,9 @@ if (isset($_POST["imgUploadSubmit"])) {
 	} else if (strlen($_FILES['imgUpload']['name']) > 0) {
 		echo "file";
 		if (move_uploaded_file($_FILES['imgUpload']['tmp_name'], "/tmp/".$_FILES['imgUpload']['name'])) {
+			echo "hi";
 			$data = file_get_contents('/tmp/'.$_FILES['imgUpload']['name']);
+			echo "hi";
 
 			$pvars = array('image' => base64_encode($data), 'key' => 'ef01658e300dbcf7aa0ecdd18a3bed7c');
 			$timeout = 30;
