cb7578ba25d4c1e263aab4d0a79db9302b91171a
Sherry Wu <shewu19@gmail.com>
2012-02-04 22:46:48 -0500

    whoops includes are relative to what is including the file

--- util/header.php
+++ util/header.php
@@ -2,7 +2,7 @@
 <html lang=en>
 <head>
 <title>Meatboard</title>
-<link rel="stylesheet" type="text/css" href="../css/main.css" />
+<link rel="stylesheet" type="text/css" href="css/main.css" />
 </head>
 <body>
 <header>
