b944243b8707a6dc7a4d89f68c33a0aeaa51f63b
Travis Hance <tjhance7@gmail.com>
2012-02-04 23:36:36 -0500

    made select form on homepage (index.php) so you can select a whiteboard to view

--- util/mysql.php
+++ util/mysql.php
@@ -3,5 +3,11 @@ $conn = mysql_connect('localhost', 'whiteboard', 'sherry');
 if(!$conn) {
 	die("mysql");
 }
-echo "OK";
+mysql_select_db('whiteboard') or die('Could not select database whiteboard');
+
+function get_whiteboards() {
+	$query = 'SELECT * FROM whiteboards';
+	$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+	return $result;
+}
 ?>
