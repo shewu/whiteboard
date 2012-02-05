65ef01e2ad25ae93ed0a809889f859d627822f7c
Sherry Wu <shewu19@gmail.com>
2012-02-05 14:03:41 -0500

    position fixed footer

--- util/footer.php
+++ util/footer.php
@@ -1,4 +1,4 @@
-<footer style="clear:left;margin:0;padding:0;">
+<footer style="position:fixed;clear:left;margin:0;padding:0;">
 &copy; 2012 Brian Hamrick, Travis Hance, Sherry Wu. All rights reserved.
 </footer>
 </body>
