4201aaeb7ad1e9791da1ce956085471be3fd923b
Sherry Wu <shewu19@gmail.com>
2012-02-05 12:39:39 -0500

    nuked br in footer; i have no idea what that was for

--- util/footer.php
+++ util/footer.php
@@ -1,5 +1,4 @@
-<br style="clear: left; margin: 0; padding: 0;" />
-<footer>
+<footer style="clear:left;margin:0;padding:0;">
 &copy; 2012 Brian Hamrick, Travis Hance, Sherry Wu. All rights reserved.
 </footer>
 </body>
