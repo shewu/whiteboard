cf5e2b8497f9d93b196a19102b7d9af4f38d1d0b
Sherry Wu <shewu19@gmail.com>
2012-02-05 01:31:17 -0500

    made better divs

--- js/wb.js
+++ js/wb.js
@@ -21,12 +21,11 @@ function createTextlet() {
 	if (e.pageX || e.pageY) {
 		posx = e.pageX;
 		posy = e.pageY;
-	}
-	else if (e.clientX || e.clientY) {
+	} else if (e.clientX || e.clientY) {
 		posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
 		posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
 	}
-	console.log("relX = " + posx + " relY = " + posy);
-	$('#canvas').append('<div class="textlet" style="top:'+posy+'px;left:'+posx+'px;">Hello</div>');
+	var $textarea = "<div rows=2 cols=20 class=textlet style='top:"+posy+"px;left:"+posx";px' onblur='this.contentEditable=false' onclick='this.contentEditable=true'></div>";
+	$('#canvas').append($textarea);
 }
 
