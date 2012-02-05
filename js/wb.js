75876acdb70c19b18a75efa27d26446e3624901a
Sherry Wu <shewu19@gmail.com>
2012-02-04 23:21:43 -0500

    forgot to add px -__________________-

--- js/wb.js
+++ js/wb.js
@@ -3,7 +3,7 @@ function resizeViewport() {
 	// keep these two values in sync with the css kthx
 	var headerHeight = 16;
 	var footerHeight = 10;
-	document.getElementById('viewport').style.height = screenViewportHeight - headerHeight - footerHeight;
+	document.getElementById('viewport').style.height = screenViewportHeight - headerHeight - footerHeight + 'px';
 	console.log("resizing to " + (screenViewportHeight - headerHeight - footerHeight));
 	return;
 }
