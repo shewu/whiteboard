3f4974dff56623450298aa5527b893a8819c349e
Sherry Wu <shewu19@gmail.com>
2012-02-04 23:24:13 -0500

    added resizing fudge factor and nuked overflow

--- js/wb.js
+++ js/wb.js
@@ -1,12 +1,9 @@
 function resizeViewport() {
 	var screenViewportHeight = document.documentElement.clientHeight;
 	// keep these two values in sync with the css kthx
-	console.log("headerHeight = " + document.getElementsByTagName('header')[0].style.height);
-	console.log("footerHeight = " + document.getElementsByTagName('footer')[0].style.height);
 	var headerHeight = 16;
 	var footerHeight = 10;
-	document.getElementById('viewport').style.height = screenViewportHeight - headerHeight - footerHeight + 'px';
-	console.log("resizing to " + (screenViewportHeight - headerHeight - footerHeight));
+	document.getElementById('viewport').style.height = screenViewportHeight - headerHeight - footerHeight - 1 + 'px';
 	return;
 }
 
